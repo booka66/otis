f209380b0093e4ca106a4b17129a1b90c604c48e
Jacob Cahoon <jacobbcahoon@gmail.com>
2026-09-22 16:34:37 -0600

    otis 0.7.1

--- Formula/otis.rb
+++ Formula/otis.rb
@@ -13,8 +13,8 @@
 class Otis < Formula
   desc "Git and GitLab from the terminal: a dashboard and pickers drawn in the terminal"
   homepage "https://github.com/booka66/otis"
-  url "https://github.com/booka66/otis.git", using: :git, tag: "v0.7.0"
-  version "0.7.0"
+  url "https://github.com/booka66/otis.git", using: :git, tag: "v0.7.1"
+  version "0.7.1"
   head "https://github.com/booka66/otis.git", using: :git, branch: "main"
 
   depends_on "ast-grep"
